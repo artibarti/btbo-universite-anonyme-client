f94d9c7b9a38de8c29aaf7d0b19c97597635fba8
artibarti <safranyartur1995@gmail.com>
2018-11-16 18:09:45 +0100

    update deps -> see in INSTALL_NOTES, create (copy paste) an example login form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 // import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 // import { MatMenuModule, MatIconModule } from '@angular/material';
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
 import { CourseService} from './shared/course/course.service'
 import { CourseListComponent } from './course-list/course-list.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
   declarations: [
@@ -20,11 +22,13 @@ import { FooterComponent } from './footer/footer.component'
     CourseListComponent,
     ToolbarComponent,
     FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MDBBootstrapModule.forRoot()
     /* BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
@@ -35,6 +39,6 @@ import { FooterComponent } from './footer/footer.component'
     MatIconModule */
   ],
   providers: [CourseService],
-  bootstrap: [AppComponent, CourseListComponent, ToolbarComponent, FooterComponent]
+  bootstrap: [AppComponent, CourseListComponent, ToolbarComponent, FooterComponent, LoginComponent]
 })
 export class AppModule { }
--- client/src/app/course-list/course-list.component.ts
+++ client/src/app/course-list/course-list.component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from "@angular/common";
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
-  styleUrls: ['./course-list.component.css']
+  styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
 
