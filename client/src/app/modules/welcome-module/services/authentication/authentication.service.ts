0073ea9df194310b99da75b12892a66c6bd8a023
Kummer156 <wmarci15@gmail.com>
2018-11-23 19:55:02 +0100

    Login router problem fixed

--- client/src/app/modules/welcome-module/services/authentication/authentication.service.ts
+++ client/src/app/modules/welcome-module/services/authentication/authentication.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpRequest, HttpResponse, HttpHeaders } from '@angular/common/http'
-import { SessionService } from '../../../../shared/services/session/session.service'
-import { Observable } from 'rxjs/Observable'
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {SessionService} from '../../../../shared/services/session/session.service'
+import {Route, Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -10,59 +10,65 @@ export class AuthenticationService {
 
   //https://github.com/cornflourblue/angular-7-registration-login-example-cli/blob/master/src/app/login/login.component.ts
 
-  hashPasswd(password: String) : String
-  {
+  hashPasswd(password: String): String {
     return password;
   }
 
-  validateRegistration(credentials) : boolean
-  {
-    const headers = new HttpHeaders (credentials);
-    const requestHeader = {                                                                                                                                                                                 
-      headers: headers, 
+  validateRegistration(credentials): boolean {
+    const headers = new HttpHeaders(credentials);
+    const requestHeader = {
+      headers: headers,
     };
 
     var url = "//" + this.sessionService.serverName + ":" + this.sessionService.portNumber + "/register";
 
     this.http.get(url, requestHeader).subscribe(response => {
-        if (response['email']) 
-        {
-            this.sessionService.currentUser.email = response['email'];
-            this.sessionService.currentUser.name = response['name'];
-            this.sessionService.currentUser.id = response['id'];          
-        } 
-        else 
-        {
-        
-        }
+      if (response['email']) {
+        this.sessionService.currentUser.email = response['email'];
+        this.sessionService.currentUser.name = response['name'];
+        this.sessionService.currentUser.id = response['id'];
+      }
+      else {
+
+      }
     });
 
     return false;
   }
 
-  validateLogin(credentials) : boolean
-  {     
 
-    const headers = new HttpHeaders (credentials);
-    const requestHeader = {                                                                                                                                                                                 
-      headers: headers, 
+  validateLogin(credentials): boolean {
+
+    const headers = new HttpHeaders(credentials);
+    const requestHeader = {
+      headers: headers,
     };
-    
+
     var url = "//" + this.sessionService.serverName + ":" + this.sessionService.portNumber + "/login";
 
-    this.http.get(url, requestHeader).subscribe(response => {
-        if (response['email']) {
-            this.sessionService.authenticated = true;
-            this.sessionService.currentUser.email = response['email'];
-            this.sessionService.currentUser.name = response['name'];
-            this.sessionService.currentUser.id = response['id'];
-        } else {
-            this.sessionService.authenticated = false;
-        }
-    });
 
-    return false;
+
+    this.http.get(url, requestHeader).subscribe(response => {
+      if (response['email']) {
+        this.sessionService.authenticated = true;
+        this.sessionService.currentUser.email = response['email'];
+        this.sessionService.currentUser.name = response['name'];
+        this.sessionService.currentUser.id = response['id'];
+      } else {
+        this.sessionService.authenticated = false;
+      }
+    },
+      error => {
+        console.log(error)
+      },
+      () => {
+        this.router.navigate(['home'])
+      });
+
+    //return this.sessionService.authenticated;
+    return true;
   }
 
-  constructor(private http: HttpClient, private sessionService: SessionService) { }
+  constructor(private http: HttpClient, private sessionService: SessionService, private router:Router) {
+  }
 }
