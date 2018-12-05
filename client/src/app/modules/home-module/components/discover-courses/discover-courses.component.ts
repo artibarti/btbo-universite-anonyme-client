e948ddd90911fe3dd4c5f31df62e66207a4a4922
artibarti <safranyartur1995@gmail.com>
2018-12-05 02:05:55 +0100

    subsribe

--- client/src/app/modules/home-module/components/discover-courses/discover-courses.component.ts
+++ client/src/app/modules/home-module/components/discover-courses/discover-courses.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../../../../shared/services/course/course.service';
+import { Course } from '../../../../shared/models/course';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-discover-courses',
@@ -12,11 +14,14 @@ export class DiscoverCoursesComponent implements OnInit {
   errorMsg: string = "Invite code is unacceptable";
   showError: boolean = false;
 
-  constructor(private courseService: CourseService) { }
+  courses: Course[] = [];
+
+  constructor(private courseService: CourseService, private route: Router) { }
 
   ngOnInit() 
   {    
     this.inviteCode = "";
+    this.showError = false;
   }
 
   onJoinClicked()
@@ -26,7 +31,10 @@ export class DiscoverCoursesComponent implements OnInit {
       this.showError = true;
     }
 
-    this.courseService.joinCourseWithInviteCode(this.inviteCode);
+    this.courseService.joinCourseWithInviteCode(this.inviteCode).then(
+      res => {
+        this.route.navigate(["/home"]);
+      });
   }
 
 }
